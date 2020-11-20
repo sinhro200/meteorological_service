15b2ea5bd9830dd30616855ca103ba1c836798e0
sinhro <sinhro-200@mail.ru>
2020-11-20 13:23:01 +0300

    added controllers and configuration for services

---
+++ src/main/kotlin/com/sinhro/meteorological_service/config/DataServicesConfiguration.kt
@@ -0,0 +1,25 @@
+package com.sinhro.meteorological_service.config
+
+import com.sinhro.meteorological_service.service.AdministrativeAreaTypeService
+import com.sinhro.meteorological_service.service.SourceService
+import org.jooq.DSLContext
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class DataServicesConfiguration(
+        @Autowired private val dslContext: DSLContext
+){
+
+
+    @Bean
+    fun administrativeAreaTypeService(): AdministrativeAreaTypeService {
+        return AdministrativeAreaTypeService(dslContext)
+    }
+
+    @Bean
+    fun sourceService(): SourceService {
+        return SourceService(dslContext)
+    }
+}
--- src/main/kotlin/com/sinhro/meteorological_service/controller/SourceController.kt
+++ src/main/kotlin/com/sinhro/meteorological_service/controller/SourceController.kt
@@ -1,32 +1,44 @@
 package com.sinhro.meteorological_service.controller
 
+import com.sinhro.meteorological_service.dto.SourceDTO
 import com.sinhro.meteorogical_service.tables.Source
-import com.sinhro.meteorological_service.SourcePOJO
+import com.sinhro.meteorological_service.service.SourceService
 import org.jooq.DSLContext
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
 @RestController
+@RequestMapping("/api/source")
 class SourceController(
-        @Autowired private val dsl: DSLContext
+        @Autowired private val sourceService: SourceService
 ){
 
-    @GetMapping("/api/source")
-    fun getSources(): List<SourcePOJO> {
-        val sources = dsl.select(Source.SOURCE.SOURCE_ID,Source.SOURCE.TITLE)
-                .from(Source.SOURCE)
-                .fetchInto(SourcePOJO::class.java)
-        return sources
+    @GetMapping
+    fun getSources(): List<SourceDTO> {
+        return sourceService.getAllData()
     }
 
-    @PostMapping("/api/source")
-    fun postSources(
-            @RequestBody source : SourcePOJO
+    @PostMapping
+    fun postSource(
+            @RequestBody source : SourceDTO
     ): String {
-        val sources = dsl.insertInto(Source.SOURCE)
-                .columns(Source.SOURCE.SOURCE_ID,Source.SOURCE.TITLE)
-                .values(source.id,source.title)
-                .execute()
-        return "success"
+        sourceService.insertData(source)
+        return "done"
+    }
+
+    @PutMapping
+    fun changeSource(
+            @RequestBody source : SourceDTO
+    ): String {
+        sourceService.updateData(source)
+        return "done"
+    }
+
+    @DeleteMapping
+    fun deleteSource(
+            @RequestBody source: SourceDTO
+    ): String {
+        sourceService.deleteData(source)
+        return "done"
     }
 }
